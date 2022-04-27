aa7929759369cf9e41ed79156873dcd17f078cd4
binh.nd <vnt2908@gmail.com>
2022-04-27 23:30:33 +0700

    update

--- src/components/shared/layout/header.tsx
+++ src/components/shared/layout/header.tsx
@@ -1,20 +1,24 @@
-const Header = () => (
-  <div className="px-5 py-3 bg-sky-900 flex justify-between items-center">
-    <a href="" className="text-2xl text-white font-bold">
-      Logo
-    </a>
-    <ul className="flex gap-x-8">
-      <li className="text-white font-semibold flex items-center">Token</li>
-      <li className="text-white font-semibold flex items-center">Voucher</li>
-      <li className="text-white font-semibold flex items-center">About</li>
-      <li className="text-white font-semibold flex items-center">Contact</li>
-      <li>
-        <button className="bg-teal-600 text-white rounded px-5 py-1 font-semibold">
-          Connect wallet
-        </button>
-      </li>
-    </ul>
-  </div>
-)
+import { useDisclosure } from "@chakra-ui/react";
+const Header = () => {
+	const { onOpen } = useDisclosure();
+	return (
+		<div className="px-5 py-3 bg-sky-900 flex justify-between items-center">
+			<a href="" className="text-2xl text-white font-bold">
+				Logo
+			</a>
+			<ul className="flex gap-x-8">
+				<li className="text-white font-semibold flex items-center">Token</li>
+				<li className="text-white font-semibold flex items-center">Voucher</li>
+				<li className="text-white font-semibold flex items-center">About</li>
+				<li className="text-white font-semibold flex items-center">Contact</li>
+				<li>
+					<button onClick={onOpen} className="bg-teal-600 text-white rounded px-5 py-1 font-semibold">
+						Connect wallet
+					</button>
+				</li>
+			</ul>
+		</div>
+	);
+};
 
-export default Header
+export default Header;
--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -1,39 +1,35 @@
-// Set of helper functions to facilitate wallet setup
-
-import { BASE_BSC_SCAN_URL, BASE_URL } from 'config'
-
 /**
  * Prompt the user to add BSC as a network on Metamask, or switch to BSC if the wallet is on a different network
  * @returns {boolean} true if the setup succeeded, false otherwise
  */
 export const setupNetwork = async () => {
-  const provider = window.ethereum
-  if (provider) {
-    const chainId = parseInt(process.env.REACT_APP_CHAIN_ID || '1666700000', 10)
-    try {
-      await provider.request({
-        method: 'wallet_addEthereumChain',
-        params: [
-          {
-            chainId: `0x${chainId.toString(16)}`,
-            chainName: 'Binance Smart Chain Mainnet',
-            nativeCurrency: {
-              name: 'BNB',
-              symbol: 'bnb',
-              decimals: 18,
-            },
-            rpcUrls: [process.env.REACT_APP_NODE],
-            blockExplorerUrls: [`${BASE_BSC_SCAN_URL}/`],
-          },
-        ],
-      })
-      return true
-    } catch (error) {
-      console.error('Failed to setup the network in Metamask:', error)
-      return false
-    }
-  } else {
-    console.error("Can't setup the BSC network on metamask because window.ethereum is undefined")
-    return false
-  }
-}
+	const provider = window.ethereum;
+	if (provider) {
+		const chainId = parseInt(process.env.REACT_APP_CHAIN_ID || "1666700000", 10);
+		try {
+			await provider.request({
+				method: "wallet_addEthereumChain",
+				params: [
+					{
+						chainId: `0x${chainId.toString(16)}`,
+						chainName: "Harmony Mainnet",
+						nativeCurrency: {
+							name: "ONE",
+							symbol: "ONE",
+							decimals: 18,
+						},
+						rpcUrls: [process.env.REACT_APP_NODE],
+						blockExplorerUrls: [process.env.REACT_APP_BLOCK_EXPLORER_URL],
+					},
+				],
+			});
+			return true;
+		} catch (error) {
+			console.error("Failed to setup the network in Metamask:", error);
+			return false;
+		}
+	} else {
+		console.error("Can't setup the BSC network on metamask because window.ethereum is undefined");
+		return false;
+	}
+};
